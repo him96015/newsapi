9e926db845b5f2796c61e622477d8c03d08974ff
himanshuparihar\Himanshu <himanshupari007@gmail.com>
2020-08-02 17:47:21 +0530

    Security Confiugrations

---
+++ src/main/java/org/cis/cis/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package org.cis.cis.security;
+
+import org.cis.cis.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private MyUserDetailsService myuserDetailsService;
+	
+	//Authorization
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myuserDetailsService);
+						//		auth.inMemoryAuthentication()  In memory authentication
+						//			.withUser("user")       
+						//			.password("user")
+						//			.roles("USER")
+						//			.and()
+						//			.withUser("admin")
+						//			.password("admin")
+						//			.roles("ADMIN");
+	}
+	
+	//Authentication(always done with HTTP)
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
